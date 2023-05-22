a833de81af251ce1df018494da16d4149203d756
bartoszswitalski <bartoszswitalski@icloud.com>
2023-05-22 12:05:22 +0200

    WIP: quick commit

--- libs/frontend/domain/src/lib/weeks.ts
+++ libs/frontend/domain/src/lib/weeks.ts
@@ -1,9 +1,9 @@
 export type DatesRange = { startDate: Date; endDate: Date };
 
 export const WEEKS: Record<'1' | '2' | '3' | '4' | '5', DatesRange> = {
-    '1': { startDate: new Date('2023-05-15T10:00:00Z'), endDate: new Date('2023-05-22T10:00:00Z') },
-    '2': { startDate: new Date('2023-05-22T10:00:00Z'), endDate: new Date('2023-05-29T10:00:00Z') },
-    '3': { startDate: new Date('2023-05-29T10:00:00Z'), endDate: new Date('2023-06-05T10:00:00Z') },
-    '4': { startDate: new Date('2023-06-05T10:00:00Z'), endDate: new Date('2023-06-12T10:00:00Z') },
-    '5': { startDate: new Date('2023-06-12T10:00:00Z'), endDate: new Date('2023-06-18T10:00:00Z') },
+    '1': { startDate: new Date('2023-05-15T21:59:59Z'), endDate: new Date('2023-05-21T22:00:00Z') },
+    '2': { startDate: new Date('2023-05-21T21:59:59Z'), endDate: new Date('2023-05-28T22:00:00Z') },
+    '3': { startDate: new Date('2023-05-28T21:59:59Z'), endDate: new Date('2023-06-04T22:00:00Z') },
+    '4': { startDate: new Date('2023-06-04T21:59:59Z'), endDate: new Date('2023-06-11T22:00:00Z') },
+    '5': { startDate: new Date('2023-06-11T21:59:59Z'), endDate: new Date('2023-06-18T22:00:00Z') },
 };
