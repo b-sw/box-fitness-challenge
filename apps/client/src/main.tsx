efe1bf4061d1b6598e45ce4513f1a2d347ff2251
bartoszswitalski <bartoszswitalski@icloud.com>
2023-10-27 16:53:55 +0200

    WIP: quick commit

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,7 +3,7 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './app/app';
 
-axios.defaults.baseURL = 'https://boxfc.bieda.it/';
+axios.defaults.baseURL = 'https://tracelytics.toadres.pl/';
 // axios.defaults.baseURL = 'http://localhost:3333/';
 
 ReactDOM.render(
