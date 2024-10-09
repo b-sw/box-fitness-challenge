0379dcdd8866208dc46063aa8466512601d75c25
bartoszswitalski <bartoszswitalski@icloud.com>
2024-10-09 21:58:17 +0200

    backend

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -3,8 +3,8 @@ import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom';
 import { App } from './app/app';
 
-axios.defaults.baseURL = 'http://localhost:3333/';
-// axios.defaults.baseURL = 'https://boxfc.bieda.it/';
+// axios.defaults.baseURL = 'http://localhost:3333/';
+axios.defaults.baseURL = 'https://boxfc.bieda.it/';
 
 ReactDOM.render(
     <StrictMode>
