323f7d9a2ec5eb4902437c426b6f9931bc122ef6
bartoszswitalski <bartoszswitalski@icloud.com>
2024-10-14 21:43:58 +0200

    switch to serverless

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -4,7 +4,8 @@ import * as ReactDOM from 'react-dom';
 import { App } from './app/app';
 
 // axios.defaults.baseURL = 'http://localhost:3333/';
-axios.defaults.baseURL = 'https://api.moveforukraine.com/';
+// axios.defaults.baseURL = 'https://api.moveforukraine.com/';
+axios.defaults.baseURL = 'https://h50k4rb9n4.execute-api.eu-west-1.amazonaws.com/dev/';
 
 ReactDOM.render(
     <StrictMode>
