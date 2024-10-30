edf2218596ffcc080dbb515675e475fde4dc7ae8
bswitalski <bswitalski@box.com>
2024-10-30 11:50:19 +0100

    extend date range

--- libs/frontend/domain/src/lib/weeks.ts
+++ libs/frontend/domain/src/lib/weeks.ts
@@ -7,6 +7,6 @@ export type Week = {
 };
 
 export const WEEKS: Map<Week['id'], Week> = new Map([
-    [1, { id: 1, startDate: dayjs('2024-10-15T00:00:00Z'), endDate: dayjs('2024-10-28T23:59:59Z') }],
+    [1, { id: 1, startDate: dayjs('2024-10-15T00:00:00Z'), endDate: dayjs('2024-11-28T23:59:59Z') }],
     [99, { id: 2, startDate: dayjs('2024-10-01T21:59:59Z'), endDate: dayjs('2024-10-15T00:00:00Z') }],
 ]);
